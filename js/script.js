656b9272c64a1accdf5f576e67174f4073935c1b
yungdenzel <oyomafia@gmail.com>
2019-12-16 18:36:18 +0100

    personal

---
+++ js/script.js
@@ -0,0 +1,7 @@
+    
+//   document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+//   if(textArray.length) setTimeout(type, newTextDelay + 250);
+//   });
+  
+  
+
