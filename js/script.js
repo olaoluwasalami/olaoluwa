d04f24a06f0d71934bb6659396beef4b28bdb269
yungdenzel <oyomafia@gmail.com>
2019-12-26 19:42:13 +0100

    master

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ navBars = () => {
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = [" A Software Engineer", "A React Developer", ];
+const textArray = [" A Software Engineer", "A Full Stack Developer", ];
 const typingDelay = 100;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
@@ -46,29 +46,6 @@ setTimeout(type, typingDelay + 1100);
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
 if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
-
-// // Work carousel
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-// showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-// showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-// var i;
-// var slides = document.getElementsByClassName("mySlides");
-// var dots = document.getElementsByClassName("dot");
-// if (n > slides.length) {slideIndex = 1}
-// if (n < 1) {slideIndex=slides.length} for (i=0; i < slides.length; i++) { slides[i].style.display="none" ; } for (i=0;
-//   i < dots.length; i++) { dots[i].className=dots[i].className.replace(" active", "" ); }
-//   slides[slideIndex-1].style.display="block" ; dots[slideIndex-1].className +=" active" ; }
 
 
 
